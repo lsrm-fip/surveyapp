8519b39c3b59764ccf1508ea1a7091ea0db3badd
farhan <farhanhamidlubis@gmail.com>
2023-10-27 04:22:35 +0700

    gen_user_response debug

--- summary/management/commands/gen_user_response.py
+++ summary/management/commands/gen_user_response.py
@@ -45,7 +45,7 @@ class Command(BaseCommand):
             raise CommandError('File "{}" does not exist'.format(options["csv"]))
 
 
-        value_list = ["Tidak Mampu - 1","Kurang Mampu - 2","Cukup Mampu - 3","Mampu - 4","Sangat Mampu - 5","Sepenuhnya Mampu - 6"]
+        value_list = ['tidak_mampu_-_1','kurang_mampu_-_2','cukup_mampu_-_3','mampu_-_4','sangat_mampu_-_5','Sepenuhnya_mampu_-_6']
 
         for data_dict in models.get("Survey", []):
             user = User.objects.get(email=data_dict["user_email"])

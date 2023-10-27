996203fbdc55858f251ab638109e70bffff767b4
farhan <farhanhamidlubis@gmail.com>
2023-10-27 09:40:30 +0700

    fix issue on dashboard url

--- summary/urls.py
+++ summary/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from djf_surveys.admins import views as admin_views
 
 
 urlpatterns = [
-    path("", views.summary, name="summary")
+    path("", views.summary, name="summary"),
+    path("surveys/dashboard/", admin_views.AdminSurveyListView.as_view(), name="dashboard"),
 ]
